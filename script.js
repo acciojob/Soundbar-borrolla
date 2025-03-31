26a67df6c6bbf1438e2689f2f8437a983682ff4e
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-31 14:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 //your JS code here. If required.
 //const buttons = document.querySelectorAll(".btn");
 //const stopbutton = document.queryselector(".stop");
+document.getElementById('sound1').addEventListener('click', function() {
+  document.getElementById('sound1Audio').play();
+});
 
-let currentSound = null; // This will hold the current playing sound
+document.getElementById('sound2').addEventListener('click', function() {
+  document.getElementById('sound2Audio').play();
+});
 
-function playSound(soundName) {
-  // If there is a sound already playing, stop it
-  if (currentSound) {
-    currentSound.pause();
-    currentSound.currentTime = 0; // This resets the sound to the beginning
-  }
-
-  // Create a new audio object and start playing the sound
-  currentSound = new Audio(`sounds/${soundName}.mp3`);
-  currentSound.play();
-}
-
-function stopAllSounds() {
-  // If there is a sound playing, stop it
-  if (currentSound) {
-    currentSound.pause();
-    currentSound.currentTime = 0; // This resets the sound to the beginning
-  }
-}
+document.querySelector('.stop').addEventListener('click', function() {
+  var sounds = document.getElementsByTagName('audio');
+  for(i=0; i<sounds.length; i++) sounds[i].pause();
+});
