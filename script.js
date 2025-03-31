8ad792e08807f4ace947ca86e7e5cecf657908e9
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-31 18:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 //your JS code here. If required.
 //const buttons = document.querySelectorAll(".btn");
 //const stopbutton = document.queryselector(".stop");
-document.getElementById('sound1').addEventListener('click', function() {
-  document.getElementById('sound1Audio').play();
+const sounds = {
+	applause: 'sounds/applause.mp3',
+	boo : 'sounds/boo.mp3',
+	gasp:  'sounds/gasp.mp3',
+	tada : 'sounds/tada.mp3',
+	victory : 'sounds/victory.mp3',
+	wrong : 'sounds/wrong.mp3',
+};
+
+//function to play sound
+const playSound = (sound) => {
+	const audio = new Audio(sound);
+	audio.play();
+};
+
+//add event listener to buttons
+Object.keys(sounds).forEach(key) => {
+	document.getElementById(key).addEventListener('click', () => playSound(sounds[key]));
 });
 
-document.getElementById('sound2').addEventListener('click', function() {
-  document.getElementById('sound2Audio').play();
+
+//stop button functionality
+document.getElementById('stop').addEventListener('click', () => {
+	const audioElements = document.querySelectorAll('audio');
+	audioElements.forEach(audio) => audio.pause());
 });
 
-document.querySelector('.stop').addEventListener('click', function() {
-  var sounds = document.getElementsByTagName('audio');
-  for(i=0; i<sounds.length; i++) sounds[i].pause();
-});
 
 
 
