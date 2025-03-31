470ecb41201f6c4fbfa0b3bd4c2303a6eb4fb2d4
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-31 13:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let currentAudio;
+function playSound(soundName){
+	stopAllSounds();//stop any current audio play
+	currentAudio = new Audio(`sounnds/${soundName}.mp3`);
+	currentAudio.play();
+}
+
+function stopAllSounds(){
+
+ if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+	
+}
+
+
+
+
+
+
+
+
+
+
+
